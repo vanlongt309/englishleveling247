a9697592bd14dbee15f2059145d3831e9ffc18df
vanlongt309 <136171139+vanlongt309@users.noreply.github.com>
2025-05-01 18:54:00 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import VerticalFlashcardGallery from './VerticalFlashcardGallery.tsx';
 import NavigationBarBottom from './navigation-bar-bottom.tsx';
+import VerticalFlashcardGallery from './VerticalFlashcardGallery.tsx';
 import Profile from './profile.tsx';
 
 const container = document.getElementById('root');
