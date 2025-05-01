0d0445b00397ef63155d465589dee6a9edf60381
vanlongt309 <136171139+vanlongt309@users.noreply.github.com>
2025-05-01 10:25:58 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,7 @@
-// src/index.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import VerticalFlashcardGallery from './VerticalFlashcardGallery';
-import './index.css'; // nếu có custom styles
 
-// Tìm thẻ <div id="root"></div> trong public/index.html
 const container = document.getElementById('root');
-if (!container) {
-  throw new Error('Root container not found in HTML');
-}
-
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <VerticalFlashcardGallery />
-  </React.StrictMode>
-);
+if (!container) throw new Error('Root not found');
+createRoot(container).render(<VerticalFlashcardGallery />);
