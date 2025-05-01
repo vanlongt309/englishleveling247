48a00e392fc41db8809a24806df28387c616d23a
vanlongt309 <136171139+vanlongt309@users.noreply.github.com>
2025-05-01 17:53:40 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import VerticalFlashcardGallery from './VerticalFlashcardGallery.tsx';
+import NavigationBarBottom from './navigation-bar-bottom.tsx';
+import Profile from './profile.tsx';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root not found');
